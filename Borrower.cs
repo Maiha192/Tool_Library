ffb68158a9b7478ed651bcc9e7e5f03f2b18c619
maiha <maiha.219@gmail.com>
2023-09-04 10:25:19 +1000

    Update Borrower class

--- Borrower.cs
+++ Borrower.cs
@@ -3,14 +3,49 @@ namespace ToolLibrary
 {
 	public class Borrower
 	{
-	
-		public string FullName { get; set; }
-		public string PhoneNumber { get; set; }
-		public Borrower(string fullName, string phoneNumber)
+        private string firstName;
+        private string lastName;
+        private int mobile;
+
+		public string FirstName
 		{
-			this.FullName = fullName;
-			this.PhoneNumber = phoneNumber;
+		    get { return firstName;}
+		    set { value = firstName;}
 		}
-	}
+
+		public string LastName
+		{
+		    get { return lastName;}
+		    set { value = lastName;}
+		}
+
+		public int Mobile
+		{
+		    get { return mobile;}
+		    set { value = mobile;}
+		}
+
+		public Borrower(string lastName, string firstName, int mobileNumber)
+		{
+		    this.lastName = lastName;
+		    this.firstName = firstName;
+		    this.mobile = mobileNumber;
+		}
+
+		public int CompareTo(Borrower another)
+		{
+		    if (this.LastName.CompareTo(another.LastName)< 0)
+		        return -1;
+		    else
+		        if (this.LastName.CompareTo(another.LastName) == 0)
+		            return this.FirstName.CompareTo(another.FirstName);
+		        else
+		            return 1;
+		}
+
+		public override string ToString()
+		{
+		    return (firstName + " " + lastName + " " + mobile.ToString() + "\n");
+		}
 }
 
